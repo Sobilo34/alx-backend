0ad41e0ee0c4292e54ac1a8a22e7b062f8bee142
Sobilo34 <bilalsolih60@gmail.com>
2024-07-24 20:44:04 +0100

    update the code implemetation

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,10 +1,9 @@
-from base_caching import BaseCaching
-
 #!/usr/bin/env python3
 """
 A class LFUCache that inherits from
 BaseCaching and is a caching system
 """
+from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
@@ -13,8 +12,9 @@ class LFUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
-        self.lfu = []
+        self.cache_data = {}
         self.lfu_count = {}
+        self.usage_order = []
 
     def put(self, key, item):
         """
@@ -28,17 +28,20 @@ class LFUCache(BaseCaching):
             self.lfu_count[key] += 1
         else:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                min_count = min(self.lfu_count.values())
-                lfu_keys = [k for k, v in self.lfu_count.items() if v == min_count]
-                lfu_key = min(self.lfu, key=lambda x: self.lfu.index(x) if x in self.lfu else float('inf'))
-                self.lfu.remove(lfu_key)
+                # Find the least frequently used key
+                lfu_key = min(self.lfu_count, key=self.lfu_count.get)
+                # If multiple keys have the same frequency, use the LRU policy
+                lfu_keys = [k for k, v in self.lfu_count.items() if v == self.lfu_count[lfu_key]]
+                if len(lfu_keys) > 1:
+                    lfu_key = min(lfu_keys, key=lambda k: self.usage_order.index(k))
                 del self.cache_data[lfu_key]
                 del self.lfu_count[lfu_key]
+                self.usage_order.remove(lfu_key)
                 print("DISCARD: " + lfu_key)
 
             self.cache_data[key] = item
             self.lfu_count[key] = 1
-            self.lfu.append(key)
+            self.usage_order.append(key)
 
     def get(self, key):
         """
@@ -48,7 +51,7 @@ class LFUCache(BaseCaching):
             return None
 
         self.lfu_count[key] += 1
-        self.lfu.remove(key)
-        self.lfu.append(key)
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
 
         return self.cache_data[key]
