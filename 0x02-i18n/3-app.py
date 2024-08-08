4a40ce87caf0b0d4d51be6aa51b0cc8bf47758ad
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 02:54:40 +0100

    remove _

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A Flask app configured with Flask-Babel for i18n support."""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
