786320056c392ecd6df99aa11b686c2b3ef2e9b8
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 01:46:44 +0100

    Add get_locale function

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""A Flask app configured with Flask-Babel for i18n support."""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """A congif class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Find best language match
+    """
+    return request.accept_languages.best_match(app.config('LANGUAGES'))
+
+@app.route('/')
+def index() -> str:
+    """
+    The index function that render 0-app.py
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
