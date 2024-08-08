77dab0e42e3f47d6135fbbd8ec0ed0b615071152
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 02:36:23 +0100

    back to initial

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python3
-"""
-A Flask application configured with Flask-Babel for i18n support.
-"""
-
+"""A Flask app configured with Flask-Babel for i18n support."""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config:
-    """
-    Configuration class for the Flask app.
-    """
+    """A config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app: Flask = Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(Config)
-
-babel: Babel = Babel(app)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
     """
-    Determine the best match with our supported languages.
-
-    Returns:
-        str: The best match language.
+    Find best language match
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
@@ -36,13 +27,10 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """
-    Route that renders the index page.
-
-    Returns:
-        str: The rendered HTML content of the index page.
+    The index function that render 0-app.py
     """
     return render_template('3-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
