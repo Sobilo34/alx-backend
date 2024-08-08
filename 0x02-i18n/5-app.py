0c751edcd1d89a9d96ad6bf4d191eeab34a71690
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 03:30:09 +0100

    update get_locale function

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -32,6 +32,8 @@ def get_locale() -> str:
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
