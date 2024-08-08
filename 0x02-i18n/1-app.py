5ff50387837a14ea33c94471dc89466ed8684ee2
Sobilo34 <bilalsolih60@gmail.com>
2024-08-08 01:35:44 +0100

    edit the typo error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,14 +6,14 @@ from flask_babel import Babel
 
 class Config:
     """A congif class"""
-    LANGUAUGE = ["en", "fr"]
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-babel = Babel(app)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
